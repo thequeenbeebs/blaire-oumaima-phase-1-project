f1790b73e01ae94f513759dc624d5bf47bfd7382
Blaire Baker <bakerblaire@gmail.com>
2020-12-02 17:13:59 -0600

    fixed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,13 +20,13 @@ list2 = List.create(name: "things to get", shopping_or_wish: "shopping list", us
 gift1 = Gift.create(name: "Apple Watch", price: 399.99, status: "Not Purchased")
 gift2 = Gift.create(name: "Cashmere Scarf", price: 124.89, status: "Purchased")
 gift3 = Gift.create(name: "Nike Running Shoes", price: 189.99, status: "Purchased")
-gift4 = Gift.create(name: "PS5", price: 500.00), status: "Not Purchased"
+gift4 = Gift.create(name: "PS5", price: 500.00, status: "Not Purchased")
 gift5 = Gift.create(name: "Ugg Slippers", price: 79.95, status: "Not Purchased")
 gift6 = Gift.create(name: "Birthstone Necklace", price: 42.00, status: "Not Purchased")
 gift7 = Gift.create(name: "Le Labo Eau de Parfum", price: 192, status: "Not Purchased")
 gift8 = Gift.create(name: "L.L Bean Beanie", price: 16.96, status: "Not Purchased")
 gift9 = Gift.create(name: "16 Piece Dinner Set", price: 256.00, status: "Not Purchased")
-gift10 = Gift.create(name: "Bose Soundlink Speaker", price: 99.00, status, "Not Purchased")
+gift10 = Gift.create(name: "Bose Soundlink Speaker", price: 99.00, status: "Not Purchased")
 
 add_to_lists1 = AddToList.create(gift_id: gift1.id, list_id: list1.id)
 add_to_lists2 = AddToList.create(gift_id: gift3.id, list_id: list2.id)
